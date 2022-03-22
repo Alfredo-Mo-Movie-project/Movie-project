f0cdd2c4e98a9765d31d94331866e016bc4753f9
AlfredoGaitan <gaitan.alfredo78@gmail.com>
2022-03-22 16:00:19 -0500

    add design in HTML

--- js/movie-project.js
+++ js/movie-project.js
@@ -2,7 +2,7 @@ function getMovies(){
 	fetch('https://toothsome-outgoing-order.glitch.me/movies').then(response => response.json()).then(function (response) {
 		document.getElementById('movies').innerHTML = ''
 		response.forEach(function (movie) {
-			$('#movies').html(document.getElementById('movies').innerHTML += `<div>${movie.title}</div><div>${movie.rating}</div><div>${movie.id}</div><button id="${movie.id}" type="submit" class="delete">Delete</button>`)
+			$('#movies').html(document.getElementById('movies').innerHTML += `<div class="card"><div>${movie.title}</div><div>${movie.rating}</div><div>${movie.id}</div><button id="${movie.id}" type="submit" class="delete">Delete</button></div>`)
 		})
 	}).then(function (){
 		let deleteOptions = {
